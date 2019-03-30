0c5d9da06fc72e698af79bedcc03c17ec7fcd155
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-03-30 19:34:24 +0530

    Learn to create AppBar

---
+++ AppBar/flutter_one/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  build(context){
+    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Flutter'),),),);
+  }
+}
