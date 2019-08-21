8b7fae347d571bb1ac192444e07f3c75186479c4
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-21 22:12:58 +0530

    First One
    Hello world

---
+++ Flutter/1/flutter_app_1/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+      Center(
+        child: Text(
+          "Hello Flutter",
+          textDirection: TextDirection.ltr,
+        ),
+      )
+  );
+}
