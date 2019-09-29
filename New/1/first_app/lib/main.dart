e7c6f6042751bbc14c500dc088ec6e27c4922172
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-29 17:37:52 +0530

    Updated

--- New/1/first_app/lib/main.dart
+++ New/1/first_app/lib/main.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    Center(
-      child: Text(
-          "First App",textDirection: TextDirection.ltr
+    MaterialApp(
+      title: "My Flutter Application",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Application")
+        ),
+        body: Material(
+          color: Colors.blue,
+          child:Center(
+            child: Text(
+                "First App",textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  color: Colors.white,
+                  backgroundColor:Colors.blue[200],
+              ),
+            ),
+          ),
+        ),
       ),
     )
   );
