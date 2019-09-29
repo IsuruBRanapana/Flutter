2502ec6d95bdf90c0b76b5db2b983c32eb4968ef
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-29 17:14:33 +0530

    Restarted

---
+++ New/1/first_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    Center(
+      child: Text(
+          "First App",textDirection: TextDirection.ltr
+      ),
+    )
+  );
+}
