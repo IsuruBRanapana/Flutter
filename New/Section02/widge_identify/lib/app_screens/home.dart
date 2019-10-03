e6fc87f8d6b2ea9a131ced3e0da537de57b8062d
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-03 16:32:17 +0530

    Updated

--- New/Section02/widge_identify/lib/app_screens/home.dart
+++ New/Section02/widge_identify/lib/app_screens/home.dart
@@ -11,18 +11,55 @@ class Home extends StatelessWidget{
         padding: EdgeInsets.only(left: 4.0,right: 4.0,top: 10.0),
         //width: 200.0,
         //height: 500.0,
-        child: Text("ABCD",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.white,
-            decoration: TextDecoration.none,
-            fontSize: 36,
-            fontFamily:'Raleway',
-            fontWeight: FontWeight.w300,
-            backgroundColor: Colors.lightBlue[800]
-          ),
-        ),
+        child: airTable()
       ),
     );
   }
+
+  air(String airline, String place){
+    return Row(
+      children: <Widget>[
+        Expanded(child:
+          Text(
+            airline,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.none,
+                fontSize: 36,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700,
+                backgroundColor: Colors.lightBlue[800]
+            ),
+          )
+        ),
+        Expanded(child:
+          Text(
+            place,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.none,
+                fontSize: 20,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w300,
+                backgroundColor: Colors.lightBlue[800]
+            ),
+          )
+        ),
+      ],
+    );
+  }
+
+  airTable(){
+    return Column(
+      children: <Widget>[
+        air("Sri Lankan", "Sri Lanka to German"),
+        air("Oman Air", "Sri Lanka to Oman"),
+        air("Emirates", "US to Sri Lanka")
+      ],
+    );
+  }
+
 }
+
