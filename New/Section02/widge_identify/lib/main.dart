814e150197ef4fb8634bfb75c41ae66b27a11a92
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-04 19:00:22 +0530

    Updated

--- New/Section02/widge_identify/lib/main.dart
+++ New/Section02/widge_identify/lib/main.dart
@@ -5,7 +5,36 @@ void main(){
   runApp(
       MaterialApp(
         title: "Identify the widgets",
-        home: Home(),
+        home: Scaffold(
+          body: getListView(),
+        ),
       )
   );
+}
+
+Widget getListView(){
+  var list=ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(
+          Icons.landscape
+        ),
+        title: Text(
+            "Landscape"
+        ),
+        subtitle: Text(
+          "Thid is landscape"
+        ),
+        trailing: Icon(
+          Icons.access_alarm
+        ),
+        onTap: (){
+
+        },
+      )
+    ],
+  );
+
+  return list;
+
 }
