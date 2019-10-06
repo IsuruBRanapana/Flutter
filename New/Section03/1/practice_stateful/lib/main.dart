741390f9561852752ec96b08c74de954b641c250
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-06 17:04:59 +0530

    Updated

---
+++ New/Section03/1/practice_stateful/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      title:"The Stateful Widget",
+      home: CitiWidget(),
+    )
+  );
+}
+
+class CitiWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FavouriteCitiWidget();
+  }
+}
+
+class _FavouriteCitiWidget extends State<CitiWidget>{
+  String nameCity;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "StateFul Widget"
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+}
