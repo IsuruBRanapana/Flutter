4e32d30c33da18b8dd3b55c0d9632fd777b6eeb7
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-07 14:37:30 +0530

    Updated

--- New/Section03/1/interest_calculator/lib/main.dart
+++ New/Section03/1/interest_calculator/lib/main.dart
@@ -19,6 +19,7 @@ class ICal extends StatefulWidget{
 
 class _ICalState extends State<ICal>{
   var _currencies=['Rupees','Pounds','Dollars'];
+  final _minimumPadding=5.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,12 +29,53 @@ class _ICalState extends State<ICal>{
         ),
       ),
       body: Container(
+        margin: EdgeInsets.all(_minimumPadding),
         child: Column(
           children: <Widget>[
+            getImageAssets(),
+
+            Padding(
+                padding: EdgeInsets.only(top: 5.0,bottom: 5.0),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Principle",
+                      hintText: "Enter Principle like eg:-5000",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))
+                  ),
+                )
+            ),
+
+            Padding(
+                padding: EdgeInsets.only(top: 5.0,bottom: 5.0),
+                child:TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Rate of Interest",
+                      hintText: "In percent",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))
+                  ),
+                )
+            ),
 
           ],
         ),
       ),
+    );
+  }
+
+  Widget getImageAssets(){
+    AssetImage assetImage=AssetImage('images/cur.png');
+    Image image=Image(
+      width: 125.0,
+      height: 125.0,
+      image: assetImage,
+    );
+
+    return Container(
+      child: image,
     );
   }
 
