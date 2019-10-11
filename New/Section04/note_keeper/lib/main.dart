3fa4b1e4cb1751acb60265befbd0d3085cb37e42
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-11 15:34:59 +0530

    Updated

---
+++ New/Section04/note_keeper/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/note_list.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Note-Keeper",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: NoteList(),
+    );
+  }
+}
---
+++ New/Section04/note_keeper/lib/screens/note_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return NoteListState();
+  }
+}
+
+class NoteListState extends State<NoteList> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+      body: getListOfNotes(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        tooltip: "Add Note",
+        child: Icon(
+          Icons.add
+        ),
+      ),
+    );
+  }
+
+  ListView getListOfNotes() {
+    return ListView.builder(
+        itemCount: count,
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Icon(Icons.keyboard_arrow_right),
+              ),
+              title: Text(
+                "Dummy Tile",
+              ),
+              subtitle: Text("Dummy date"),
+              trailing: Icon(
+                Icons.delete,
+                color: Colors.grey,
+              ),
+              onTap: (){
+
+              },
+            ),
+          );
+        });
+  }
+}
