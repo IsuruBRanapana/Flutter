20a8ae9e761e1c36fa2b4a1a9a26a53ef2f01d1e
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-12 16:30:14 +0530

    Updated

--- New/Section04/note_keeper/lib/utils/database_helper.dart
+++ New/Section04/note_keeper/lib/utils/database_helper.dart
@@ -44,4 +44,42 @@ class DatabaseHelper{
   void _createDb(Database db, int newVersion) async{
     await db.execute('CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, $colDescription TEXT, $colPriority INTEGER, $colDate TEXT)');
   }
+
+  //Fetch
+  Future<List<Map<String,dynamic>>> getNoteMapList() async{
+    Database db=await this.database;
+
+    var result=await db.query(noteTable,orderBy: '$colPriority ASC');
+    return result;
+  }
+
+  //insert
+  Future<int> insertNote(Note note)async{
+    Database db=await this.database;
+    var result=await db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  //update
+
+  Future<int> updateNote(Note note) async{
+    var db=await this.database;
+    var result=await db.update(noteTable, note.toMap(), where: '$colId=?',whereArgs: [note.id]);
+    return result;
+  }
+
+  //delete
+  Future<int> deleteNote(int id) async{
+    var db=await this.database;
+    var result=await db.rawDelete('DELETE FROM $noteTable WHERE $colId=$id');
+    return result;
+  }
+
+  //get number of result
+  Future<int> getCount() async{
+    Database db=await this.database;
+    List<Map<String,dynamic>> x=await db.rawQuery('SELECT COUNT (*) FROM $noteTable');
+    int result=Sqflite.firstIntValue(x);
+    return result;
+  }
 }
