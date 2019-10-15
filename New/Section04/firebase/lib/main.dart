ca47ef3c1d271bdeabcd1447faf9160963d6ee2a
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-10-15 23:27:43 +0530

    updated

--- New/Section04/firebase/lib/main.dart
+++ New/Section04/firebase/lib/main.dart
@@ -1,30 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
   }
 }
 
-class _MyAppState extends State<MyApp>{
-  String name,profession,message;
-  List<DropdownMenuItem> items=[
+class _MyAppState extends State<MyApp> {
+  GlobalKey<FormState> _key = GlobalKey();
+  bool _autoValidate = false;
+  String name, profession, message;
+  List<DropdownMenuItem> items = [
     DropdownMenuItem(
-      child:Text(
-        'Student'
-      ),
+      child: Text('Student'),
     ),
     DropdownMenuItem(
-      child:Text(
-          'Professor'
-      ),
+      child: Text('Professor'),
     ),
   ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,29 +36,68 @@ class _MyAppState extends State<MyApp>{
           ),
         ),
         body: SingleChildScrollView(
+          padding: EdgeInsets.all(15.0),
           child: Container(
-            child: formUI(),
+            child: Form(
+              child: formUI(),
+              autovalidate: _autoValidate,
+            ),
           ),
         ),
       ),
     );
-
   }
-  Widget formUI(){
+
+  Widget formUI() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         TextFormField(
           decoration: InputDecoration(hintText: 'Name'),
           maxLength: 32,
+          onSaved: ((val) {
+            name = val;
+          }),
+          validator: validateName,
         ),
         DropdownButton(
           items: items,
-          onChanged: ((val){
-            profession=val;
+          onChanged: ((val) {
+            profession = val;
           }),
-        )
+          hint: Text('Profession'),
+          value: profession,
+        ),
+        TextFormField(
+          decoration: InputDecoration(hintText: 'Message'),
+          maxLength: 256,
+          maxLines: 5,
+          onSaved: ((val) {
+            message = val;
+          }),
+          validator: validateMessage,
+        ),
+        RaisedButton(
+          onPressed: _sendToServer,
+          child: Text('Send'),
+        ),
       ],
     );
   }
 
+  _sendToServer() {
+    if(_key.currentState.validate()){
+      _key.currentState.save();
+    }else{
+      _autoValidate=true;
+    }
+  }
+
+  String validateName(String val) {
+    return val.length == 0 ? 'Enter Name First' : null;
+  }
+
+  String validateMessage(String val) {
+    return val.length == 0 ? 'Enter Message' : null;
+  }
 }
