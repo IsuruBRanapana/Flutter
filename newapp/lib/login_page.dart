9acbc83f398fc31de1ddce0fefb431491df11928
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-11-18 19:32:28 +0530

    Updated

---
+++ newapp/lib/login_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+enum FormType { login, register }
+
+class _LoginPageState extends State<LoginPage> {
+  final formkey = GlobalKey<FormState>();
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+  bool validateAndSave() {
+    final form = formkey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void validateAndSubmit() async {
+    if (validateAndSave()) {
+      try {
+        if (_formType == FormType.login) {
+          AuthResult result = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(email: _email, password: _password);
+          FirebaseUser user = result.user;
+        }else if(_formType==FormType.register){
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email,password: _password);
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+
+  void moveToRegister() {
+    formkey.currentState.reset()
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter log in'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: formkey,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: buildInputs() + submitButtons()),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildInputs() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(labelText: 'Email'),
+        validator: (value) => value.isEmpty ? 'Email must Include' : null,
+        onSaved: (value) => _email = value,
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Password must Include' : null,
+        onSaved: (value) => _password = value,
+      )
+    ];
+  }
+
+  List<Widget> submitButtons() {
+    if (_formType == FormType.login) {
+      return [
+        RaisedButton(
+          onPressed: () {
+            validateAndSubmit();
+          },
+          child: Text('Log in'),
+        ),
+        FlatButton(
+          child: Text('Create an account'),
+          onPressed: moveToRegister,
+        )
+      ];
+    } else {
+      return [
+        RaisedButton(
+          onPressed: () {
+            validateAndSubmit();
+          },
+          child: Text('Create an Account'),
+        ),
+        FlatButton(
+          child: Text('Have an account'),
+          onPressed: moveToLogin,
+        )
+      ];
+    }
+  }
+}
