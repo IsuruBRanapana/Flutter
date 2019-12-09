bd791722561098999b35e9d4d7edbfc0447a066f
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2019-12-09 20:15:43 +0530

    Updated

--- localization/my_localization/lib/app_localizations.dart
+++ localization/my_localization/lib/app_localizations.dart
@@ -3,3 +3,26 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+class AppLocalizations{
+  final Locale locale;
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of (BuildContext context){
+    return Localizations.of<AppLocalizations>(context,AppLocalizations);
+  }
+
+  Map<String,String> _localizedStrings;
+  Future<bool> load() async{
+    String jsonString=await rootBundle.loadString('lang/${locale.languageCode}.json');
+    Map<String,dynamic> jsonMap= json.decode(jsonString);
+
+    _localizedStrings = jsonMap.map((key,value){
+      return MapEntry(key, value.toString());
+    });
+    return true;
+  }
+
+  String translate(String key){
+    return _localizedStrings[key];
+  }
+}
